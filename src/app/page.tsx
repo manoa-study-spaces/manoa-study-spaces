ff4f34f80a7b6e9425bf561b241e24b51f94edc3
Chloe Whitaker <cagcbw@gmail.com>
2026-04-16 10:00:29 -1000

    go color girl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,10 @@ const Home = () => (
     <Container id="home-page" fluid className="py-3">
       <Row className="align-middle text-center">
         <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to Manoa Study Spaces!</h1>
-          <h2>~ your new home for studying with (or without) friends~</h2>
-          <p>This website is designed exclusively for UH Manoa students to safely explore and utilize study spaces. 
+          <h1 style={{ color: '#3e7969' }}>Welcome to Manoa Study Spaces!</h1>
+          <h2 style={{ color: '#A7C9BF' }}>~ your new home for studying with (or without) friends~</h2>
+
+          <p style={{ color: '#3e7969' }}>This website is designed exclusively for UH Manoa students to safely explore and utilize study spaces. 
             Sign up today to browse the latest listings and top recommendations from fellow students.
             Discover various locations around UH Manoa and all their ammenities to fit your needs!</p>
         </Col>
