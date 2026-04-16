039ea9ef37ef4a546f281403ceda4df3556c4462
Chloe Whitaker <cagcbw@gmail.com>
2026-04-16 09:22:00 -1000

    get

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -4,7 +4,23 @@ export const AddStuffSchema = Yup.object({
   buildingName: Yup.string().required(),
   roomNumber: Yup.string(),
   times:
-  pictures:
+
+
+  pictures: Yup.mixed<FileList>()
+    .test('minFiles', 'At least one photo is required', (value) => value && value.length >= 1)
+    .test('maxFiles', 'You can upload at most 9 photos', (value) => value && value.length <= 9)
+    .test('validFileTypes', 'Only JPG and PNG formats are allowed', (value) => {
+      if (!value) {
+        return false;
+      }
+      for (let i = 0; i < value.length; ++i) {
+        const file = value[i];
+        if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
+          return false;
+        }
+      }
+
+
   occupancy: Yup.string().oneOf(['empty', 'moderate', 'crowded']).required(),
   foodAllowed: Yup.string().oneOf(['permitted', 'prohibited', 'water okay']).required(),
   noiseLevel: Yup.string().oneOf(['quiet', 'moderate', 'loud']).required(),
