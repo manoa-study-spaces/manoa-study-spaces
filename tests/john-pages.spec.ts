49c2f16a6f2e0018fcb6b439c799c2defcf6ac9d
Fubumaa <brianiki@hawaii.edu>
2026-05-07 17:54:52 -1000

    Update john-pages.spec.ts

--- tests/john-pages.spec.ts
+++ tests/john-pages.spec.ts
@@ -1,27 +1,27 @@
 import { test, expect } from './auth-utils';
 
 test.slow();
-test('can authenticate a specific user', async ({ getUserPage }) => {
+test('can authenticate a specific user', async ({ page }) => {
 
   // Call the getUserPage fixture with users signin info to get authenticated session for user
-  const customUserPage = await getUserPage('john@foo.com', 'changeme');
+  // const customUserPage = await getUserPage('john@foo.com', 'changeme');
 
   // Navigate to the home page and wait for post-login indicator
-  await customUserPage.goto('https://manoa-study-spaces-main.vercel.app');
+  await page.goto('https://manoa-study-spaces-main.vercel.app');
   await expect(
-    customUserPage
+    page
   ).toHaveTitle('Manoa Study Spaces');
 
   // Now check for navigation links and headings
   await expect(
-    customUserPage).toHaveURL('https://manoa-study-spaces-main.vercel.app/');
+    page).toHaveURL('https://manoa-study-spaces-main.vercel.app/');
   await expect(
-    customUserPage).toHaveURL('https://manoa-study-spaces-main.vercel.app/');
+    page).toHaveURL('https://manoa-study-spaces-main.vercel.app/');
 
-  await customUserPage.goto('https://manoa-study-spaces-main.vercel.app/');
+  await page.goto('https://manoa-study-spaces-main.vercel.app/');
 
   await expect(
-    customUserPage).toHaveURL('https://manoa-study-spaces-main.vercel.app/');
+    page).toHaveURL('https://manoa-study-spaces-main.vercel.app/');
 
 
 });
