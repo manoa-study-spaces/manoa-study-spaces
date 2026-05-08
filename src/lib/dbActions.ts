1e2ba59a3c02bb845ea4ccf807d932083551defe
Fubumaa <brianiki@hawaii.edu>
2026-05-07 17:05:47 -1000

    Update dbActions.ts

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -104,7 +104,7 @@ export async function addListing(data: {
   }
 
   // Return the newly created listing so the client can update local state/localStorage.
-  return newListing;
+  redirect(`/list`);
 }
 
 export async function createReview(data: {
