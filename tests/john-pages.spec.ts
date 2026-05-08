55af728ceed93c01c8587125b1c3e8e10312bc4b
Fubumaa <brianiki@hawaii.edu>
2026-05-07 14:59:26 -1000

    edited tests

--- tests/john-pages.spec.ts
+++ tests/john-pages.spec.ts
@@ -4,10 +4,10 @@ test.slow();
 test('can authenticate a specific user', async ({ getUserPage }) => {
 
   // Call the getUserPage fixture with users signin info to get authenticated session for user
-  const customUserPage = await getUserPage('brianiki@hawaii.edu', '102938');
+  const customUserPage = await getUserPage('john@foo.com', 'changeme');
 
   // Navigate to the home page and wait for post-login indicator
-  await customUserPage.goto('https://manoa-study-spaces-powwrfvsx-brians-projects-1f312bea.vercel.app/');
+  await customUserPage.goto('https://manoa-study-spaces-main.vercel.app/');
   await expect(
     customUserPage.getByRole('link', { name: 'Study Spaces' })
   ).toBeVisible({ timeout: 10000 });
